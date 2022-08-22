87a5937987eeb2d065885be8f39c87908e34472b
DevExpressExampleBot <fakeEmail>
2022-08-22 12:57:41 +0400

    Source auto update [skip ci]

--- VB/RequestLocationInformation/Form1.vb
+++ VB/RequestLocationInformation/Form1.vb
@@ -1,29 +1,34 @@
-﻿Imports System
+Imports System
 Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraMap
 
 Namespace RequestLocationInformation
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
         Private latitude As Double
+
         Private longitude As Double
-        Private Const yourBingKey As String = "INSERT_YOUR_BING_KEY_HERE"
+
+        Const yourBingKey As String = "INSERT_YOUR_BING_KEY_HERE"
+
         Private geocodeProvider As BingGeocodeDataProvider
+
         Private map As MapControl
 
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
             PrepareMap()
-            AddHandler geocodeProvider.LocationInformationReceived, AddressOf geocodeProvider_LocationInformationReceived
+            AddHandler geocodeProvider.LocationInformationReceived, New LocationInformationReceivedEventHandler(AddressOf geocodeProvider_LocationInformationReceived)
         End Sub
 
-        #Region "#CustomRequestUI"
-        Private Sub requestLocation_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles requestLocation.Click
+#Region "#CustomRequestUI"
+        Private Sub requestLocation_Click(ByVal sender As Object, ByVal e As EventArgs)
             If GetLocationArguments() Then
                 geocodeProvider.RequestLocationInformation(New GeoPoint(latitude, longitude), 0)
             End If
@@ -31,27 +36,28 @@ Namespace RequestLocationInformation
 
         Private Function GetLocationArguments() As Boolean
             latitude = If(String.IsNullOrEmpty(tbLatitude.Text), 0, Double.Parse(tbLatitude.Text))
-            If (latitude > 90) OrElse (latitude < -90) Then
+            If latitude > 90 OrElse latitude < -90 Then
                 MessageBox.Show("Latitude must be less than or equal to 90 and greater than or equal to - 90. Correct the input value.")
                 Return False
             End If
 
             longitude = If(String.IsNullOrEmpty(tbLongitude.Text), 0, Double.Parse(tbLongitude.Text))
-            If (longitude > 180) OrElse (longitude < -180) Then
+            If longitude > 180 OrElse longitude < -180 Then
                 MessageBox.Show("Longitude must be less than or equal to 180 and greater than or equal to - 180. Correct the input value.")
                 Return False
             End If
+
             Return True
         End Function
-        #End Region ' #CustomRequestUI
 
-        #Region "#CustomRecievedUI"
+#End Region  ' #CustomRequestUI
+#Region "#CustomRecievedUI"
         Private Sub geocodeProvider_LocationInformationReceived(ByVal sender As Object, ByVal e As LocationInformationReceivedEventArgs)
             Dim result As GeocodeRequestResult = e.Result
-
             If result.Locations.Length > 0 Then
                 NavigateTo(result.Locations(0).Location)
             End If
+
             DisplayResult(result)
         End Sub
 
@@ -61,64 +67,51 @@ Namespace RequestLocationInformation
         End Sub
 
         Private Sub DisplayResult(ByVal result As GeocodeRequestResult)
-            Dim resultList As New StringBuilder("")
+            Dim resultList As StringBuilder = New StringBuilder("")
             Dim resCounter As Integer = 1
-
             If result.ResultCode = RequestResultCode.Success Then
-
                 For Each locations As LocationInformation In result.Locations
-                    resultList.Append(String.Format("Request Result {0}:" & ControlChars.Lf, resCounter))
-                    resultList.Append(String.Format(locations.EntityType & ControlChars.Lf))
-                    resultList.Append(String.Format(locations.Address.FormattedAddress & ControlChars.Lf))
-                    resultList.Append(String.Format("Coordinates: {0}" & ControlChars.Lf, locations.Location))
-                    resultList.Append(String.Format("______________________________" & ControlChars.Lf))
+                    resultList.Append(String.Format("Request Result {0}:" & Microsoft.VisualBasic.Constants.vbLf, resCounter))
+                    resultList.Append(String.Format(locations.EntityType & Microsoft.VisualBasic.Constants.vbLf))
+                    resultList.Append(String.Format(locations.Address.FormattedAddress & Microsoft.VisualBasic.Constants.vbLf))
+                    resultList.Append(String.Format("Coordinates: {0}" & Microsoft.VisualBasic.Constants.vbLf, locations.Location))
+                    resultList.Append(String.Format("______________________________" & Microsoft.VisualBasic.Constants.vbLf))
                     resCounter += 1
-                Next locations
+                Next
+
                 tbResult.Text = resultList.ToString()
             End If
 
-            If result.Locations.Length = 0 Then
-                tbResult.Text = "The Bing Geocode service does not work for this location."
-            End If
+            If result.Locations.Length = 0 Then tbResult.Text = "The Bing Geocode service does not work for this location."
         End Sub
-        #End Region ' #CustomRecievedUI
 
+#End Region  ' #CustomRecievedUI
         Private Sub PrepareMap()
             ' Create a map control.
             map = New MapControl()
-
             ' Specify the map position on the form.           
             map.Dock = DockStyle.Fill
-
             ' Add the map control to the window.
             Me.Controls.Add(map)
-
             ' Bring the map to the front.
             map.BringToFront()
-
-'            #Region "#CreateGeocodeProvider"
+#Region "#CreateGeocodeProvider"
             ' Create an image tiles layer and add it to the map.
-            Dim tilesLayer As New ImageTilesLayer()
+            Dim tilesLayer As ImageTilesLayer = New ImageTilesLayer()
             map.Layers.Add(tilesLayer)
-
             ' Create an  information layer and add it to the map.
-            Dim infoLayer As New InformationLayer()
+            Dim infoLayer As InformationLayer = New InformationLayer()
             map.Layers.Add(infoLayer)
-
             ' Create a Bing data provider and specify the Bing key.
-            Dim bingProvider As New BingMapDataProvider()
+            Dim bingProvider As BingMapDataProvider = New BingMapDataProvider()
             tilesLayer.DataProvider = bingProvider
             bingProvider.BingKey = yourBingKey
-
             ' Create a geocode data provider.
             geocodeProvider = New BingGeocodeDataProvider()
             infoLayer.DataProvider = geocodeProvider
             geocodeProvider.BingKey = yourBingKey
             geocodeProvider.MaxVisibleResultCount = 1
-'            #End Region ' #CreateGeocodeProvider
-
+#End Region  ' #CreateGeocodeProvider
         End Sub
-
     End Class
-
 End Namespace
--- VB/RequestLocationInformation/Properties/AssemblyInfo.vb
+++ VB/RequestLocationInformation/Properties/AssemblyInfo.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to get information about a geographical point using the Microsoft Bing Geocode web service
 ' 
 ' This example demonstrates how to obtain information about a geographical point
@@ -31,7 +31,6 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E5061
-
 ' Developer Express Code Central Example:
 ' How to get information about a geographical point using the Microsoft Bing Geocode web service
 ' 
@@ -65,7 +64,6 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E5061
-
 ' Developer Express Code Central Example:
 ' How to get information about a geographical point using the Microsoft Bing Geocode web service
 ' 
@@ -99,7 +97,6 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E5061
-
 ' Developer Express Code Central Example:
 ' How to get information about a geographical point using the MS Bing Geocode data provider
 ' 
@@ -133,31 +130,26 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E5061
-
 Imports System.Reflection
-Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
 
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
-<Assembly: AssemblyTitle("RequestLocationInformation")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyConfiguration("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("RequestLocationInformation")>
-<Assembly: AssemblyCopyright("Copyright ©  2014")>
-<Assembly: AssemblyTrademark("")>
-<Assembly: AssemblyCulture("")>
-
+<Assembly:AssemblyTitle("RequestLocationInformation")>
+<Assembly:AssemblyDescription("")>
+<Assembly:AssemblyConfiguration("")>
+<Assembly:AssemblyCompany("")>
+<Assembly:AssemblyProduct("RequestLocationInformation")>
+<Assembly:AssemblyCopyright("Copyright ©  2014")>
+<Assembly:AssemblyTrademark("")>
+<Assembly:AssemblyCulture("")>
 ' Setting ComVisible to false makes the types in this assembly not visible 
 ' to COM components.  If you need to access a type in this assembly from 
 ' COM, set the ComVisible attribute to true on that type.
-<Assembly: ComVisible(False)>
-
+<Assembly:ComVisible(False)>
 ' The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("eb496a9d-08bf-43c7-9b0a-7870d6e2fb48")>
-
+<Assembly:Guid("eb496a9d-08bf-43c7-9b0a-7870d6e2fb48")>
 ' Version information for an assembly consists of the following four values:
 '
 '      Major Version
@@ -168,5 +160,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 ' [assembly: AssemblyVersion("1.0.*")]
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly:AssemblyVersion("1.0.0.0")>
+<Assembly:AssemblyFileVersion("1.0.0.0")>
